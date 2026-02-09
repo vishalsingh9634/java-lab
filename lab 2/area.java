8a582e7381dbb54566088ab29a94071030c72ef9
Vishal Singh <vishalsinghbrn@gmail.com>
2026-02-09 18:40:41 +0530

    area

--- lab 2/area.java
+++ lab 2/area.java
@@ -1,3 +1,20 @@
 public class area {
-    
+    double calculate(double r){
+        return 3.14*r*r;
+    }
+    double calculate(double l,double b){
+        return l*b;
+
+    }
+
+    int calculate(int s){
+        return s*s;
+    }
+
+    public static void main(String[] args) {
+        area a = new area();
+        System.out.println("area of circle :"+a.calculate(5.5));
+        System.out.println("area of rectangle :"+a.calculate(5,4));
+        System.out.println("area of Square :"+a.calculate(5));
+    }
 }
